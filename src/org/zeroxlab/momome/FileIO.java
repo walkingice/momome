d5a84d2eadb631d7543a8cd9435922d4c1b22025
Julian_Chu <walkingice@0xlab.org>
2012-06-19 18:08:07 +0800

    Add new interface: FileIO
    use implementations of this interface to save content to file.
    
    it could be clear text or encrypted.

---
+++ src/org/zeroxlab/momome/FileIO.java
@@ -0,0 +1,24 @@
+/*
+ * Authored By Julian Chu <walkingice@0xlab.org>
+ *
+ * Copyright (c) 2012 0xlab.org - http://0xlab.org/
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.zeroxlab.momome;
+
+public interface FileIO {
+    public boolean save(String key, String outputPath, CharSequence content);
+    public CharSequence read(String key, String inputPath);
+}
