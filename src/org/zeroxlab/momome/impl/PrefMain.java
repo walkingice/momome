3fb4e958e73cae2f59d7bdc0c28742f8c6a7962f
Julian_Chu <walkingice@0xlab.org>
2012-06-28 03:06:37 +0800

    implement export data

--- src/org/zeroxlab/momome/impl/PrefMain.java
+++ src/org/zeroxlab/momome/impl/PrefMain.java
@@ -19,11 +19,14 @@
 package org.zeroxlab.momome.impl;
 
 import org.zeroxlab.momome.R;
+import org.zeroxlab.momome.data.Item;;
 import org.zeroxlab.momome.Momo;
 import org.zeroxlab.momome.MomoApp;
 import org.zeroxlab.momome.MomoModel;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.preference.Preference;
@@ -31,6 +34,11 @@ import android.preference.Preference.OnPreferenceClickListener;
 import android.preference.PreferenceActivity;
 import android.util.Log;
 import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.util.List;
 
 public class PrefMain extends PreferenceActivity implements Momo {
 
@@ -54,19 +62,59 @@ public class PrefMain extends PreferenceActivity implements Momo {
         deletePref.setOnPreferenceClickListener(listener);
         changePref.setOnPreferenceClickListener(listener);
 
-        if (mModel.status() != DataStatus.OK
-                || mModel.status() != DataStatus.FILE_IS_EMPTY) {
-
+        if (mModel.status() != DataStatus.OK) {
             exportPref.setEnabled(false);
             importPref.setEnabled(false);
             changePref.setEnabled(false);
         }
     }
 
+    private void askExportData() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        File file = new File(EXTERNAL_DIR, FILENAME);
+        builder.setTitle("Export data?");
+        builder.setMessage("write to: " + file.getPath());
+        builder.setPositiveButton(android.R.string.yes,
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        if (which == DialogInterface.BUTTON_POSITIVE) {
+                            onExportData();
+                        }
+                    }
+                });
+        builder.show();
+    }
+
+    private void onExportData() {
+        List<Item> items = mModel.getItems();
+        File output = new File(EXTERNAL_DIR, FILENAME);
+        if (!output.getParentFile().canWrite()) {
+            showToast("Cannot write to " + output.getPath());
+            return;
+        }
+
+        try {
+            FileOutputStream fos = new FileOutputStream(output);
+            boolean success = mModel.saveHelper(fos, items);
+            if (success) {
+                showToast("Done!");
+            } else {
+                showToast("Failed on writing");
+            }
+        } catch (Exception e) {
+            showToast("Failed");
+            e.printStackTrace();
+        }
+    }
+
+    private void showToast(CharSequence msg) {
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+    }
+
     class DataActionListener implements OnPreferenceClickListener {
         public boolean onPreferenceClick(Preference preference) {
             if (preference.getKey().equals(KEY_EXPORT_DATA)) {
-                Toast.makeText(PrefMain.this, "Export not implement yet",Toast.LENGTH_SHORT).show();
+                askExportData();
             } else if (preference.getKey().equals(KEY_IMPORT_DATA)) {
                 Toast.makeText(PrefMain.this, "Import not implement yet",Toast.LENGTH_SHORT).show();
             } else if (preference.getKey().equals(KEY_DELETE_DATA)) {
