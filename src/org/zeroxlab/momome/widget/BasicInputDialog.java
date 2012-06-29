bb6875f1f7eef983875de4f9c3774f8a8535cc73
Julian_Chu <walkingice@0xlab.org>
2012-06-29 11:43:34 +0800

    Each EditText should be single line

--- src/org/zeroxlab/momome/widget/BasicInputDialog.java
+++ src/org/zeroxlab/momome/widget/BasicInputDialog.java
@@ -59,6 +59,7 @@ public class BasicInputDialog extends AlertDialog {
 
     private void init(Context context) {
         mEditText = new EditText(context);
+        mEditText.setSingleLine(true);
         setCancelable(true);
         setView(mEditText, PADDING, PADDING, PADDING, PADDING);
     }
